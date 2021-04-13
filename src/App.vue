0446a7da03479b3c625eeda5f3b32b695b8b74e9
陈蕾 <chenlei@techsight.com>
2021-04-13 16:13:13 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
-    <a
-        href="http://localhost:8501/?AxMxSessionToken=abcdefghigklmn"
-        target="_blank"
-    >跳转到tangshan管理后台</a
-    >
+    <!--<a-->
+    <!--    href="http://localhost:8501/?AxMxSessionToken=abcdefghigklmn"-->
+    <!--    target="_blank"-->
+    <!--&gt;跳转到tangshan管理后台</a-->
+    <!--&gt;-->
     <!--<button @click="toA">跳转到A页面</button>-->
     <!--<button @click="toB">跳转到B页面</button>-->
     <router-view></router-view>
